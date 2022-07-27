a6d2c9fb64bb4bd962ffd208435b96a82eac3870
DanielPham311 <phamquocduy3112003@gmail.com>
2022-07-27 09:34:35 +0700

    add bubbleSort

---
+++ Sorting/bubbleSort.h
@@ -0,0 +1,14 @@
+#include <algorithm>
+
+template <typename T>
+void bubbleSort(T a[], int left, int right)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+ 
+        // Last i elements are already
+        // in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                std::swap(arr[j], arr[j + 1]);
+}
