42537ed9860734e246b242412c6e8cd7906a1483
DanielPham311 <phamquocduy3112003@gmail.com>
2022-07-27 09:13:28 +0700

    add

---
+++ Sorting/selectionSort.h
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+void selectionSort(T[], int left, int right)
+{
+    for (int i=left, i<right; ++i)
+    {
+        int min = i;
+        for (int j=i+i; j<=right; ++j)
+        {
+            if (a[j] < a[min]) min = j;
+        }
+        swap(a[i],a[min]);
+    }
+}
